14c0c6afedf007487889aa279fac63102781d374
Avinash M <110030394+avinashMswami@users.noreply.github.com>
2024-12-07 11:31:51 +0530

    revoke dummy

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -4,9 +4,29 @@ import '../styles/Header.css';
 const Header = () => {
   return (
     <header className="header">
-      <div className="header-top">
-        <h1>LIC</h1>
-        <p>Join please the Team of LIC Development Officer to make your dreams come true</p>
+      <div className="logo-placeholder"> <img src="./assets/LIC-Logo.png" alt="LIC-Logo" srcset="" /> </div>
+      <div className="contact-info">
+        <div className="text-highlight">
+          Join the Team of M. Madhusudhanan, LIC Development Officer, Chennai, to make your dreams come true
+        </div>
+        <div className="horizontal-info">
+          <div className="search-bar">
+            <input type="text" placeholder="Search" />
+            <button>
+              <i className="fas fa-search"></i>
+            </button>
+          </div>
+          <div>Skip to main content</div>
+          <div>
+            <i className="fas fa-phone"></i> +91 9600108347
+          </div>
+          <div>
+            <i className="fab fa-whatsapp"></i> +91 9600108347
+          </div>
+          <div>
+            <i className="fas fa-map-marker-alt"></i> Branch Locator
+          </div>
+        </div>
       </div>
     </header>
   );
